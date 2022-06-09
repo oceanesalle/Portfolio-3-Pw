9bacda3e03fca22e6c6ed114c6730a9a782f9d35
oceansalle <oceanesalleiae@gmail.com>
2022-06-09 18:04:58 +0200

    header component

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'; 
+
+function Header () {
+  return (
+    <div id='home' className='container header-container'>
+      <h3>
+        <p>
+          I <span>'</span>m 
+        </p>
+        Océane
+        <h2>Full Stack Web Developer</h2>
+      </h3>
+    </div>
+  ); 
+}
+
+export default Header; 
